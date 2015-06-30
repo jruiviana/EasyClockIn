0ec133664c7a0ad240ae0c6dd253ced8362f70b3
jruiviana <jruiviana@gmail.com>
2015-06-30 20:58:20 -0300

    Initial commit

---
+++ app/src/main/java/com/mobile/shmaster/easyclockin/PhotosFragment.java
@@ -0,0 +1,26 @@
+package com.mobile.shmaster.easyclockin;
+
+/**
+ * Created by JOSE RUI on 29/06/2015.
+ */
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PhotosFragment extends Fragment {
+
+    public PhotosFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        return rootView;
+    }
+}
